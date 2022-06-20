c7b17268c3aa02edb12449eefb18cfb488426e62
Ode42 <ode.progr@outlook.com>
2022-06-20 10:34:04 +0300

    Styling

--- src/components/Tilannetiedot.jsx
+++ src/components/Tilannetiedot.jsx
@@ -1,12 +1,46 @@
 import React, { useEffect, useState } from "react";
 import getTilannetiedot from "../services/getTilannetiedot";
+import axios from "axios";
+import env from "../env.json";
 
 export default function Tilannetiedot() {
   const [tilannetiedot, setTilannetiedot] = useState([]);
 
+  async function getTilannetiedot() {
+    try {
+      const baseURL = env.variables.baseURL;
+      const tilannetiedotURL = baseURL.concat("/tilanne/tilannetiedot");
+      const haetutTiedot = await axios.get(tilannetiedotURL);
+      setTilannetiedot(haetutTiedot.data);
+      console.log("haettu");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     getTilannetiedot();
+
+    const interval = setInterval(() => {
+      getTilannetiedot();
+    }, 10000);
+    return () => clearInterval(interval);
   }, []);
 
-  return <div className="tilannetiedot">Tilannetiedot</div>;
+  return (
+    <div className="tilannetiedot">
+      {tilannetiedot.map((tieto) => {
+        return (
+          <div className="tilannetieto" key={tieto.tieto_id}>
+            <p>{tieto.kuvaus}</p>
+            <div className="tieto-meta">
+              <p>{tieto.lahettaja}</p>
+              <p>{tieto.label}</p>
+              <p>date</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
