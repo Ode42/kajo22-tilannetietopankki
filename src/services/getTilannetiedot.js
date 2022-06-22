95be35a693fdfc27210d7bb892e556fba1c4f48b
Ode42 <ode.progr@outlook.com>
2022-06-22 11:38:54 +0300

    Code sturcturing

--- src/services/getTilannetiedot.js
+++ src/services/getTilannetiedot.js
@@ -1,13 +1,8 @@
 import axios from "axios";
-import env from "../env.json";
 
-export default async function getTilannetiedot() {
-  try {
-    const baseURL = env.variables.baseURL;
-    const tilannetiedotURL = baseURL.concat("/tilanne/tilannetiedot");
-    const tilannetiedot = await axios.get(tilannetiedotURL);
-    return tilannetiedot.data;
-  } catch (error) {
-    console.error(error);
-  }
+export default async function getTilannetiedot(getURL) {
+  const tilannetiedot = await axios
+    .get(getURL)
+    .catch((error) => console.error(error));
+  return tilannetiedot.data;
 }
